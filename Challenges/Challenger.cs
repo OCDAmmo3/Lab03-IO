665c821157b0ee391e36f54f84d4c10ee0e0726c
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-08-26 13:43:38 -0500

    Added initial files and first tests + challenge passing

---
+++ Challenges/Challenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class Challenger
+    {
+        public static int Challenge1(int val1, int val2, int val3)
+        {
+            return val1 * val2 * val3;
+        }
+    }
+}
---
+++ Lab03-IO.Tests/Challenges.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Challenges;
+using Xunit;
+
+namespace Lab03_IO.Tests
+{
+    public class Challenges
+    {
+        [Theory]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(0, 1, 1, 0)]
+        [InlineData(-1, 15, 1, -15)]
+        public static void Challenge1_should_return_product_of_given_values(int val1, int val2, int val3, int result)
+        {
+            // Act
+            int assert = Challenger.Challenge1(val1, val2, val3);
+
+            // Assert
+            Assert.Equal(result, assert);
+        }
+    }
+}
